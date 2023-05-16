7fcf9658cdb212101c3d79866d2882576a96ca35
Koi <mkaorirahadj89@gmail.com>
2023-05-16 20:58:39 +0700

    Update Driver.java

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,7 +4,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		Counter counter = new Counter();
-		System.out.println("Count:" + counter.getCount1());
+		System.out.println("Count:" + counter.getCount());
 		counter.increment();
 		System.out.println("Count:" + counter.getCount());
 		counter.decrement();
